f4ae1a144e489e2b680be9e61147d70f77103b59
Kano.c <jimmyy5121@gmail.com>
2022-01-06 15:49:28 +0800

    add eslint prettier postcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
-let port = ref(import.meta.env.VITE_PORT);
-import.meta.env.VITE_PORT
-setInterval(() => {
-  port.value++
-  console.log(port);
-}, 1000)
+  import { ref } from "vue";
+  let port = ref(import.meta.env.VITE_PORT);
+  import.meta.env.VITE_PORT;
+  setInterval(() => {
+    port.value++;
+    console.log(port);
+  }, 1000);
 </script>
 
 <template>
