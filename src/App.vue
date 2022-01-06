ef1997580531652a5985cb7ce8366c57cc1c1037
Kano.c <jimmyy5121@gmail.com>
2022-01-06 17:47:41 +0800

    安裝less 配且配置全局less

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-  import { ref } from "vue";
+  import { ref } from 'vue';
   let port = ref(import.meta.env.VITE_PORT);
   import.meta.env.VITE_PORT;
   setInterval(() => {
     port.value++;
-    console.log(port);
   }, 1000);
 </script>
 
 <template>
-  <div>vite port:</div>
-  <div>{{ port }}</div>
+  <div class="title">vite por222t:</div>
+  <div class="content">{{ port }}</div>
 </template>
+
+<style lang="less">
+  .title {
+    color: @link-color;
+  }
+  .content {
+    color: @link-color;
+  }
+</style>
